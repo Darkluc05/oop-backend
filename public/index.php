c713f8a403f6ea7fb94a15bc0b1f92212163243c
Luc <34852@ma-web.nl>
2023-11-22 16:51:59 +0100

    add status function

--- public/index.php
+++ public/index.php
@@ -2,13 +2,13 @@
 
 class Bottle
 {
-    private array $colours = ["black"];
-    private int $opacity = 100;
+    private array $colours;
+    private int $opacity = 0;
     private int $filledLevel = 0;
     private bool $open = false;
-    public function __construct()
+    public function __construct($colours = ["black"])
     {
-
+        $this->colours = $colours;
     }
     public function drink($amount): int
     {
@@ -62,6 +62,42 @@ class Bottle
         $this->colours = $newColour;
         return $this->colours;
     }
+
+
+    public function status()
+    {
+        $colourAmount = count($this->colours);
+        echo  "the $colourAmount colours for this bottle are:";
+        foreach ($this->colours as $key => $val) {
+            echo " ",$val;
+        }
+        echo "<br/>";
+        echo "the opacity is $this->opacity <br/>";
+        echo "the bottle is filled $this->filledLevel %<br/>";
+        if($this->open = false) {
+            $currentOpen = "closed";
+        } else {
+            $currentOpen = "open";
+        }
+        echo "the bottle is $currentOpen";
+    }
+
+    public function changeOpacity(int $newOpacity): int
+    {
+        if ($newOpacity < 0) {
+            $this->opacity = 0;
+        } else if ($newOpacity > 100) {
+            $this->opacity = 100;
+        } else {
+            $this->opacity = $newOpacity;
+        }
+        return $this->opacity;
+    }
+
 }
 
-$newBottle = new Bottle();
+$newBottle = new Bottle(["blue", "black"]);
+
+$newBottle->status();
+
+?>
